6655a47fb26952965cc7a35db8889c7308797e49
Nathan Griffiths <ngriffiths@postmedia.com>
2024-09-10 21:03:50 -0700

    geocode & basic candidate info shows now

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,11 @@
     import * as turf from '@turf/helpers';
     import PointsWithinPolygon from '@turf/points-within-polygon';
     import RidingDetails from '$components/RidingDetails.svelte';
-    import Select from "svelte-select"; // https://github.com/rob-balfre/svelte-select
+    import GeocodingControl from "@maptiler/geocoding-control/svelte/GeocodingControl.svelte";
 
 
     // DATA
+    import mapTilerApiKey from '$data/api-key';
     import { menuItems } from '$data/menu-items';
     import ridings from '$data/riding-boundaries-2024.js';
     const candidatesUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTk-n-FsNcDDFKdo-zB665ebijtYBNE5G9i1WflJYgStgVItlvT26XmzBn_T1Vkn2lKkYggnkVAA2UJ/pub?gid=0&single=true&output=csv';
@@ -16,15 +17,14 @@
 
 
     // VARIABLES
-    let candidateData, resultsData, value;
-    const defaultSelectValue = menuItems[0].value;
-    let latlon = [-123.19361394392898, 49.400479859367884];
+    let candidateData, resultsData;
+    const bcBbox = [-139.595032,48.302552,-113.887024,60.199227];
+    // let latlon = [-123.19361394392898, 49.400479859367884];
 
     // REACTIVE VARIABLES
     $: ridingName = '';
     $: ridingResults = [];
     $: ridingCandidates = [];
-    $: value, updateData(value);
 
     async function fetchData(url) {
         return new Promise((resolve, reject) => {
@@ -39,16 +39,13 @@
                 }
             })
         });
-        // const resp = await fetch(url);
-        // data = await resp.text();
-        // return csvParse(data);
     }
 
     function getCandidates(ridingName, data) {
         return data.filter(d => d.electoral_district === ridingName);
     }
 
-    function getResults(ridingName, data) {
+    function getRidingName(ridingName, data) {
         return data.filter(d => d.electoral_district === ridingName);
     }
 
@@ -71,46 +68,55 @@
         return ridingName;
     }
 
-    function updateData(value) {
-        if (!value || !value.value) return;
-
-        // console.log(value);
+    function handleGeocodeResults(e) {
+        // console.log(e.detail)
+        if (e.detail !== null) {
+            const latlon = e.detail.center;
+            ridingName = getRiding(latlon, ridings);
+            ridingResults = getRidingName(ridingName, resultsData);
+            ridingCandidates = getCandidates(ridingName, candidateData);
+        }
     }
+
+        // if (e.detail && e.detail.hasOwnProperty('center')) {
+        //     latlonCoords = e.detail.center
+        // } else {
+        //     // functions to get candidates, etc, etc.
+        //     console.log(latlonCoords)
+        //     updateRidingDisplay(latlonCoords)
+        // }
+    
 
     async function init() {
         // fetch candidate data
         candidateData = await fetchData(candidatesUrl);
-        console.log(candidateData);
+        // console.log(candidateData);
 
         // fetch vote result data
         resultsData = await fetchData(resultsUrl);
         // console.log(resultsData);
-
-        ridingName = getRiding(latlon, ridings);
-        ridingResults = getResults(ridingName, resultsData);
-        ridingCandidates = getCandidates(ridingName, candidateData);
-        console.log(ridingResults)
-
-        // default display selector value
-		value = defaultSelectValue;
     }
 
     onMount(init);
 </script>
 
 <header>
-    <h1>VS SvelteKit Template</h1>
+    <h1>Who’s running in my riding?</h1>
     <p class="subhead">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </header>
 
 <main>
-    <Select items={menuItems}
-        bind:value
-        change={updateData}
-        placeholder="Pick a city..."
-		showChevron="true"
-		listOpen={false}
-    />
+    <div class="geocoding">
+        <GeocodingControl 
+            apiKey={mapTilerApiKey}
+            bbox={bcBbox}
+            country='ca'
+            limit=10
+            minLength=2
+            placeholder='Lookup a street or address...'
+            on:pick={handleGeocodeResults}
+        />
+    </div>
 
     <RidingDetails 
         riding={ridingName}
@@ -132,6 +138,7 @@
     @import '$css/app.css';
 
     header {
+        border: 1px solid blue;
 		margin-bottom: 2rem;
 	}
 	header > h1 {
@@ -142,6 +149,14 @@
 		max-width: 525px;
 		text-align: center;
 	}
+    .geocoding {
+        display: block;
+        margin: 0 auto;
+        width: 250px;
+        /* position: absolute;
+        top: 10px;
+        left: 10px; */
+    }
 
     /* COMBOBOX SELECTOR */
   	:global(.svelte-select) {
